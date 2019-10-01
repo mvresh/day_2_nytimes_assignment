1cd40964c5921222cfa7d91a25d2869a905ccf91
Varun Resh Maddali <mvresh@gmail.com>
2019-10-01 18:00:39 +0530

    ny times padding again and other changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,38 +21,49 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('New York Times'),
+        title: Center(
+          child: Text('New York Times',style: TextStyle(
+              color: Colors.black
+          ),),
+        ),
+
         backgroundColor: Colors.white,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,10,0,0),
             child: Container(
                 width: double.infinity,
-                height: 30,
+                height: 34,
                 child: Text(
                 ' PM Modi seeks ideas for his IIT-Madras convocation speech',
                 style: TextStyle(
+                fontSize: 14,
                 fontWeight: FontWeight.bold,
                 color: Colors.black),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,2,0,0),
             child: Row(
               children: <Widget>[
                 Container(
                   width: 200,
                   child: Text(
-                      "In Chennai, PM Modi would also participate in the prize distribution ceremony of the Singapore-India Hackathon."),
+                      "In Chennai, PM Modi would also participate in the prize distribution ceremony of the Singapore-India Hackathon.",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+
                 ),
                 SizedBox(width: 20),
                 Container(
                   child: Image(
-                    height: 100,
+                    height: 75,
                     width: 100,
                     image: NetworkImage(
                         'https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2019/09/29/Pictures/pm-narendra-modi-smart-cities-mission_ee684f10-e2a8-11e9-93be-d8edb8f85faf.jpg'),
@@ -62,7 +73,7 @@ void main() {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,2,0,0),
             child: Row(
               children: <Widget>[
                 Container(
@@ -72,7 +83,8 @@ void main() {
                       'Politics',
                       style: TextStyle(
                         color: Colors.black,
-                        fontSize: 15,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
                       ),
                     )),
                 Container(
@@ -80,7 +92,9 @@ void main() {
                     height: 15,
                     child: Text(
                       '8h Ago',
-                      style: TextStyle(color: Colors.black),
+                      style: TextStyle(color: Colors.black,
+                      fontSize: 12,
+                        fontWeight: FontWeight.bold,),
                     )),
                 Container(
                   child: Icon(
@@ -91,33 +105,37 @@ void main() {
               ],
             ),
           ),
-          SizedBox(height: 3),
+          SizedBox(height: 12),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,10,0,0),
             child: Container(
               width: double.infinity,
-              height: 30,
+              height: 34,
               child: Text(
                 'Flooded Roads, Stranded Locals and Warning of More Rains',
                 style: TextStyle(
+                    fontSize: 14,
                     fontWeight: FontWeight.bold,
                     color: Colors.black),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,2,0,0),
             child: Row(
               children: <Widget>[
                 Container(
                   width: 200,
                   child: Text(
-                      "Patna Nagar Nigam personnel, donning yellow raincoats, could be seen at various spots trying to unclog the manholes that have been choked by polythene and debris."),
+                      "Patna Nagar Nigam personnel, donning yellow raincoats, could be seen at various spots trying to unclog the manholes that have been choked by polythene and debris.",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),),
                 ),
                 SizedBox(width: 20),
                 Container(
                   child: Image(
-                    height: 100,
+                    height: 75,
                     width: 100,
                     image: NetworkImage(
                         'https://images.news18.com/ibnlive/uploads/2019/09/Flood-Cartoon1.jpg'),
@@ -127,7 +145,7 @@ void main() {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,2,0,0),
             child: Row(
               children: <Widget>[
                 Container(
@@ -137,7 +155,8 @@ void main() {
                       'Politics',
                       style: TextStyle(
                         color: Colors.black,
-                        fontSize: 15,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
                       ),
                     )),
                 Container(
@@ -145,7 +164,9 @@ void main() {
                     height: 15,
                     child: Text(
                       '8h Ago',
-                      style: TextStyle(color: Colors.black),
+                      style: TextStyle(color: Colors.black,
+                      fontSize: 12,
+                        fontWeight: FontWeight.bold,),
                     )),
                 Container(
                   child: Icon(
@@ -156,33 +177,37 @@ void main() {
               ],
             ),
           ),
-          SizedBox(height: 3),
+          SizedBox(height: 12),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,10,0,0),
             child: Container(
               width: double.infinity,
-              height: 30,
+              height: 34,
               child: Text(
                 'Saudi crown prince denies ordering Jamal Khashoggi killing',
                 style: TextStyle(
+                    fontSize: 14,
                     fontWeight: FontWeight.bold,
                     color: Colors.black),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,2,0,0),
             child: Row(
               children: <Widget>[
                 Container(
                   width: 200,
                   child: Text(
-                      "Some think that I should know what 3 million people working for the Saudi government do daily, says Mohammed bin Salman"),
+                      "Some think that I should know what 3 million people working for the Saudi government do daily, says Mohammed bin Salman",
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),),
                 ),
                 SizedBox(width: 20),
                 Container(
                   child: Image(
-                    height: 100,
+                    height: 75,
                     width: 100,
                     image: NetworkImage(
                         'https://www.thehindu.com/news/national/73kpsr/article26333229.ece/ALTERNATES/FREE_460/TH22KRASALMAN'),
@@ -192,7 +217,7 @@ void main() {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(2.0),
+            padding: const EdgeInsets.fromLTRB(10,2,0,0),
             child: Row(
               children: <Widget>[
                 Container(
@@ -202,7 +227,8 @@ void main() {
                       'Politics',
                       style: TextStyle(
                         color: Colors.black,
-                        fontSize: 15,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
                       ),
                     )),
                 Container(
@@ -210,7 +236,9 @@ void main() {
                     height: 15,
                     child: Text(
                       '8h Ago',
-                      style: TextStyle(color: Colors.black),
+                      style: TextStyle(color: Colors.black,
+                      fontSize: 12,
+                        fontWeight: FontWeight.bold,),
                     )),
                 Container(
                   child: Icon(
